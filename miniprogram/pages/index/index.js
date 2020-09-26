24e177a1d677e865706dfbe75feede3d7bb35e9c
ldf <kfcfk1985@163.com>
2020-09-26 15:17:45 +0800

    1.获取数据库列表并显示 2.分页,一页显示3本书，滚动加载下一页 3.下拉刷新

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,120 +1,73 @@
 //index.js
 const app = getApp()
+const myDB = wx.cloud.database({
+  env: 'kfcfk1985-1gzt5yq98b147fd7'
+})
+
+const pageHeight = wx.getSystemInfoSync().windowHeight; //获取页面的高度
+const BOOK_NUMBER_PER_PAGE = 3;
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    books: [],
+    bookItemHeight: Math.floor(pageHeight / BOOK_NUMBER_PER_PAGE),
+    page: 0,
   },
-
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
+  onLoad: function () {
+ 
+    this.getBookItem(true)
   },
+  onReachBottom() {
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
+    this.getBookItem();
+  },
+  onPullDownRefresh(){
+    console.log(`这是下拉刷新`)
+
+    this.getBookItem(true)
+
+  },
+  getBookItem(init = false) {
+    let collection = myDB.collection("doubanbooks").orderBy("create_time", "desc")
+
+    if (init == false) {
+      collection = collection.skip((this.data.page) * BOOK_NUMBER_PER_PAGE)
+    }else{
       this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
+        books:[],
+        page:0,
       })
     }
-  },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
+    wx.showLoading();
+    wx.showNavigationBarLoading();
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
 
-        wx.showLoading({
-          title: '上传中',
+    collection.limit(BOOK_NUMBER_PER_PAGE).get().then(ret => {
+      console.log(`获取第${this.data.page}页数据库返回的数据`, ret)
+
+
+      if (ret.data.length > 0) {
+        this.setData({
+          page: this.data.page + 1
         })
 
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
+        this.setData({
+            books: [...this.data.books, ...ret.data],
           },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
+          () => {
+
+            console.log("图书数组信息", this.data.books)
           }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
+        )
+      } else {
+        console.log("我是有底线的")
       }
-    })
-  },
 
+
+      wx.hideLoading();
+      wx.hideNavigationBarLoading();
+      wx.stopPullDownRefresh();
+    })
+  }
 })
